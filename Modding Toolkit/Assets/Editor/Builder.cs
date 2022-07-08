6970d3dca8325f5507182834fc78a2348c43be6e
Andrey Onishchenko <loraner123@gmail.com>
2022-07-08 12:29:57 +0300

    Builder update

--- Modding Toolkit/Assets/Editor/Builder.cs
+++ Modding Toolkit/Assets/Editor/Builder.cs
@@ -3,16 +3,23 @@ using System.IO;
 
 public class AssetBundlesBuilder
 {
-    [MenuItem("Assets/Build AssetBundles")]
-    static void BuildAllAssetBundles()
+
+    static void BuildAllAssetBundles(BuildTarget buildTarget, string pathExtend)
     {
-        string assetBundleDirectory = "Assets/AssetBundles";
-        if(!Directory.Exists(assetBundleDirectory))
+        string assetBundleDirectory = "Assets/AssetBundles/" + pathExtend;
+        if (!Directory.Exists(assetBundleDirectory))
         {
             Directory.CreateDirectory(assetBundleDirectory);
         }
-        BuildPipeline.BuildAssetBundles(assetBundleDirectory, 
-                                        BuildAssetBundleOptions.None, 
-                                        BuildTarget.StandaloneWindows);
+
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory,
+            BuildAssetBundleOptions.None,
+            buildTarget);
     }
+
+    [MenuItem("Assets/Build AssetBundles/Windows")]
+    static void BuildAllAssetBundlesWindows() => BuildAllAssetBundles(BuildTarget.StandaloneWindows, "Windows");
+
+    [MenuItem("Assets/Build AssetBundles/Mac")]
+    static void BuildAllAssetBundlesMac() => BuildAllAssetBundles(BuildTarget.StandaloneOSX, "Mac");
 }
